zvolin/go-libp2p

commit 90eeff42fcd945cc347162d1ac9df0fe0ef31fe5

nat: add locks around nat

p2p/nat/nat.go
@@ -52,8 +52,9 @@ func DiscoverNAT() *NAT {
 // service that will periodically renew port mappings,
 // and keep an up-to-date list of all the external addresses.
 type NAT struct {
-	nat  nat.NAT
-	proc goprocess.Process // manages nat mappings lifecycle
+	natmu sync.Mutex
+	nat   nat.NAT
+	proc  goprocess.Process // manages nat mappings lifecycle
 
 	mappingmu sync.RWMutex // guards mappings
 	mappings  map[*mapping]struct{}
@@ -170,11 +171,13 @@ func (nat *NAT) establishMapping(m *mapping) {
 		comment = "libp2p-" + m.comment
 	}
 
+	nat.natmu.Lock()
 	newport, err := nat.nat.AddPortMapping(m.Protocol(), m.InternalPort(), comment, MappingDuration)
 	if err != nil {
 		// Some hardware does not support mappings with timeout, so try that
 		newport, err = nat.nat.AddPortMapping(m.Protocol(), m.InternalPort(), comment, 0)
 	}
+	nat.natmu.Lock()
 
 	failure := func() {
 		m.setExternalPort(0) // clear mapping

p2p/nat/mapping.go
@@ -106,7 +106,9 @@ func (m *mapping) ExternalAddr() (ma.Multiaddr, error) {
 		return nil, ErrNoMapping
 	}
 
+	m.nat.natmu.Lock()
 	ip, err := m.nat.nat.GetExternalAddress()
+	m.nat.natmu.Unlock()
 	if err != nil {
 		return nil, err
 	}

commit 417285710929ff43931858c4f32c9bb8067328e0

fix locking issue introduced in 90eeff42fcd

p2p/nat/nat.go
@@ -177,7 +177,7 @@ func (nat *NAT) establishMapping(m *mapping) {
 		// Some hardware does not support mappings with timeout, so try that
 		newport, err = nat.nat.AddPortMapping(m.Protocol(), m.InternalPort(), comment, 0)
 	}
-	nat.natmu.Lock()
+	nat.natmu.Unlock()
 
 	failure := func() {
 		m.setExternalPort(0) // clear mapping