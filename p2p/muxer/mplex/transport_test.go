7773c57d1d99075da7fb0589216a9a1906378e61
Hlib <hlibwondertan@gmail.com>
2020-03-02 13:47:11 +0200

    restructure files layout

--- p2p/muxer/mplex/transport_test.go
+++ p2p/muxer/mplex/transport_test.go
@@ -6,6 +6,6 @@ import (
 	test "github.com/libp2p/go-libp2p-testing/suites/mux"
 )
 
-func TestMultiplexTransport(t *testing.T) {
+func TestDefaultTransport(t *testing.T) {
 	test.SubtestAll(t, DefaultTransport)
 }
